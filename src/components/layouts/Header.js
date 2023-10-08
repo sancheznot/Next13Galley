4a45561d9359ba1d341d40eb989f4ec5ecbbfdf0
sancheznot <sancheznotp@gmail.com>
2023-10-07 22:53:59 -0700

    fix the header and create the first route document

--- src/components/layouts/Header.js
+++ src/components/layouts/Header.js
@@ -4,33 +4,40 @@ import { usePathname } from "next/navigation";
 import React from "react";
 import { ThemeSwitcher } from "./ThemeSwitcher";
 
-const linkSelected = "bg-orange-300 text-black p-1 rounded-lg";
+const linkSelected = "bg-orange-300 text-black p-3 rounded-lg";
 
 const Header = () => {
   const path = usePathname()
 
+  const listLink = [
+    {
+      name: "Home",
+      path: "/"
+    },
+    {
+      name: "Gallery",
+      path: "/gallery"
+    },
+    {
+      name: "Add Picture",
+      path: "/add"
+    }
+  ]
 
   return (
-    <header className="grid  grid-cols-3 grid-flow-row auto-rows-auto place-items-center  w-full dark:bg-black bg-slate-200 dark:text-white dark">
-      <ThemeSwitcher />
+    <header className="grid grid-cols-2 place-items-center  w-full dark:bg-black bg-slate-200 dark:text-white dark">
       <h1 className="text-3xl">Welcome to my Gallery</h1>
       <div className="grid grid-cols-1 w-full">
         <ul className="grid grid-rows-none grid-cols-3 w-full gap-10 p-3 py-5">
-          <li className="py-1  w-6/12">
-            <Link href={"/"} className={path === "/" ? linkSelected : `dark:bg-gray-400 text-gray-300 p-1 rounded-lg`}>
-              Home
-            </Link>
-          </li>
-          <li className="py-1 w-6/12">
-            <Link href={"/gallery"} className={path === "/gallery" ? linkSelected : `dark:bg-gray-400 text-gray-300 p-1 rounded-lg`}>
-              Gallery
-            </Link>
-          </li>
-          <li className="py-1 w-6/12">
-            <Link href={"/add"} className={path === "/add" ? linkSelected : `dark:bg-gray-400 text-gray-300 p-1 rounded-lg`}>
-              add picture
-            </Link>
-          </li>
+          {listLink.map((link, index) => {
+            return (
+              <li className="py-1 w-6/12" key={index}>
+                <Link href={link.path} className={path === link.path ? linkSelected : `dark:bg-gray-400 text-gray-300 p-3 rounded-lg`}>
+                  {link.name}
+                </Link>
+              </li>
+            )
+          })}
         </ul>
       </div>
     </header>
